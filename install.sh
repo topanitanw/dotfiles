d2fbabdfe301ef3efa7fabfb4ec8302c02fb8f92
Panitan Wongse-ammat <sassofono@hotmail.com>
2018-06-22 01:21:35 -0700

    add init.vim for nvim and update .vimrc
    - .vimrc
      - install a package to highlight only the current line on
        the current pane
      - update several options for syntastics
    - add init.vim for nvim
    - install.sh create a folder and sync init.vim

--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-WIN_BASH_DST_DIR="/home/panitanw"
-MAC_DST_DIR="/Users/panitanw"
-LINUX_DST_DIR="/home/panitanw"
-DST_DIR=${WIN_BASH_DST_DIR}
+USERNAME="pwongseammat"
+WIN_BASH_DST_DIR="/home/${USERNAME}"
+MAC_DST_DIR="/Users/${USERNAME}"
+LINUX_DST_DIR="/home/${USERNAME}"
+DST_DIR="${MAC_DST_DIR}"
 
 function fsync {
 	rsync $1 $2
 }
 
-fsync emacs/.emacs ${DST_DIR}
-fsync vim/.vimrc ${DST_DIR}
-fsync bash/.bashrc ${DST_DIR}
-fsync bash/.bash_profile ${DST_DIR}
-fsync bash/.inputrc ${DST_DIR}
-fsync .tmux.conf ${DST_DIR}
+fsync emacs/.emacs "${DST_DIR}"
+fsync vim/.vimrc "${DST_DIR}"
+fsync bash/.bashrc "${DST_DIR}"
+fsync bash/.bash_profile "${DST_DIR}"
+fsync bash/.inputrc "${DST_DIR}"
+fsync .tmux.conf "${DST_DIR}"
+fsync .pylintrc "${DST_DIR}"
+
+CONFIG_DIR="${DST_DIR}/.config"
+if [ ! -d "${CONFIG_DIR}" ]; then
+  mkdir "${CONFIG_DIR}"
+fi
+
+NVIM_DIR="${CONFIG_DIR}/nvim"
+if [ ! -d "${NVIM_DIR}" ]; then
+  mkdir "${NVIM_DIR}"
+fi
+fsync init.vim "${NVIM_DIR}"
+
+
